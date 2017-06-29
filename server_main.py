996292cc453bc4e48cf89923fbc0604d5331166a
MGilbert <mgilbert@MGilberts-MacBook-Pro.local>
2017-06-29 16:25:11 -0700

    server now finishes off round trip photo session

--- server_main.py
+++ server_main.py
@@ -3,10 +3,13 @@ import tornado.web
 import json
 import os
 import time
+import datetime
+import glob
+import shutil
 import subprocess
 import random
 from tornado import gen
-# from sh import gphoto2 as gp
+from sh import gphoto2 as gp
 
 
 PATH = os.path.join(os.path.dirname(__file__), "static")
@@ -34,7 +37,7 @@ class SnapHandler(tornado.web.RequestHandler):
         print "SNAP"
        #### just a call to take a snap
         triggerCommand = ["--trigger-capture"]
-        # gp(triggerCommand)
+        gp(triggerCommand)
 
 
 
@@ -46,16 +49,32 @@ class CleanUpHandler(tornado.web.RequestHandler):
     def get(self):
 
         print "console hit"
-        time.sleep(20)
+        # time.sleep(20)
 
         ### copy down the files off the camera
+        # getFilesCommand = ["--get-all-files"]
+        # gp(getFilesCommand)
+
 
         ### make a datetime dir on the flash drive
-
+        newSessionDirName = os.path.join(r'/media/pi/HP v125w/kikanina', datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S') )
+        os.makedirs(newSessionDirName)
         ### copy the files to the flash subdir
-
+        cwd = r'/media/pi/HP v125w/kikanina'
+        currentDir = os.getcwd()
+        for snap in glob.iglob(os.path.join(currentDir, "*.JPG")):
+            os.remove(snap)
+        downloadCommand = ["--get-all-files"]
+        # status = subprocess.Popen("gphoto2 --get-all-files", cwd=newSessionDirName,shell=True)
+        time.sleep(2)
+        gp(downloadCommand)
+        ### copy them to the right Dir
+        for snap in glob.iglob(os.path.join(currentDir, "*.JPG")):
+            shutil.copy(snap, newSessionDirName)
         ### delete the photos off the camera
-
+        clearphotos = subprocess.check_output(r"gphoto2 --folder='/store_00020001/DCIM/100CANON' -R --delete-all-files", shell=True)
+        # clearCommand = ["--folder=","/store_00020001/DCIM/100CANON" "-R", "--delete-all-files"]
+        # gp(clearCommand)
         ### return done  (Possibly a gif?)
         self.write("Done")
         self.finish()
